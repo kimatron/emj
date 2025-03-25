9d70fab82a06819c85c8e709f47b40f90118679a
kimatron <kimmykim@gmail.com>
2025-03-25 23:29:33 +0000

    Add main site styles

--- emjcamera/portfolio/admin.py
+++ emjcamera/portfolio/admin.py
@@ -5,34 +5,45 @@ from .models import Category, Album, Photo, Contact, SiteSettings
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
 
 class PhotoInline(admin.TabularInline):
     model = Photo
     extra = 1
+    fields = ('image', 'title', 'order', 'is_featured')
 
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'date_created', 'is_featured', 'is_published')
+    list_display = ('title', 'category', 'date_created', 'photo_count', 'is_featured', 'is_published')
     list_filter = ('category', 'is_featured', 'is_published')
     prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'description')
     inlines = [PhotoInline]
+    list_editable = ('is_featured', 'is_published')
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'album', 'date_uploaded', 'is_featured')
+    list_display = ('id', 'title', 'album', 'date_uploaded', 'is_featured')
     list_filter = ('album', 'is_featured')
+    search_fields = ('title', 'description')
+    list_editable = ('is_featured',)
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject', 'date_submitted', 'is_responded')
-    list_filter = ('is_responded',)
+    list_filter = ('is_responded', 'date_submitted')
     readonly_fields = ('name', 'email', 'phone', 'subject', 'message', 'date_submitted')
+    search_fields = ('name', 'email', 'subject', 'message')
+    list_editable = ('is_responded',)
 
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Basic Information', {
-            'fields': ('site_title', 'tagline', 'about_text', 'about_image')
+        ('Site Information', {
+            'fields': ('site_title', 'tagline')
+        }),
+        ('About Page', {
+            'fields': ('about_text', 'about_image')
         }),
         ('Contact Information', {
             'fields': ('contact_info',)
