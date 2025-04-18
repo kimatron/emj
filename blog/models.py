c7ec6c88e1bb02a0f9a284650a692e7d7184ca45
kimatron <kimmykim@gmail.com>
2025-04-18 13:27:34 +0100

    Add blog catergory, post, comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
+class BlogCategory(models.Model):
+    """Category for blog posts"""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        verbose_name_plural = "Blog Categories"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class BlogTag(models.Model):
+    """Tags for blog posts"""
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, blank=True)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class BlogPost(models.Model):
+    """Blog post model"""
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(BlogTag, blank=True, related_name='posts')
+    featured_image = models.ImageField(upload_to='blog/', blank=True)
+    excerpt = models.TextField(blank=True, help_text="A short summary of the post")
+    content = RichTextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    
+    class Meta:
+        ordering = ['-published_at', '-created_at']
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog_post_detail', kwargs={'slug': self.slug})
+
+class Comment(models.Model):
+    """Comments on blog posts"""
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    website = models.URLField(blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['created_at']
+    
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post.title}"
 # Create your models here.
