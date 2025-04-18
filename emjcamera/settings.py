bb04477fbf1b0117102f06d56396c8d407c8235e
kimatron <kimmykim@gmail.com>
2025-04-18 13:26:46 +0100

    Install ckeditor for blog posting rich text

--- emjcamera/settings.py
+++ emjcamera/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'ckeditor_uploader',
 
     # Custom apps
     'portfolio',
